4f88b5d18b11a25dc7c06cb744c62e692a3e7338
920603 <46135961+920603@users.noreply.github.com>
2019-04-30 23:58:26 +0200

    finished 6th FCC project

---
+++ 06.random-quote-machine/precache-manifest.30ef7542ca305fac49637fd4b8201a41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade5c07d41e09a22aef78c42252d3272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2a8345a876753659900",
+    "url": "/static/css/main.37f3fa3e.chunk.css"
+  },
+  {
+    "revision": "453ded4a47a1a1a2e733",
+    "url": "/static/js/2.774c5952.chunk.js"
+  },
+  {
+    "revision": "e2a8345a876753659900",
+    "url": "/static/js/main.b412d58f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
